13cceafa291486252ff8d046a452bec6fc9e2fdf
Grzegorz <grzegorz.gejlo@cyfroweszkoly.pl>
2024-10-28 08:25:13 +0100

    Utworzenie pytań (imie, nazwisko, wiek) oraz tworzenie obiektu z odpowiedzi

--- dataLogger.mjs
+++ dataLogger.mjs
@@ -5,5 +5,14 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-
-
+rl.question("Podaj swoje imie: ", function(imie) {
+    rl.question("Podaj swoje nazwisko: ", function(nazwisko){
+        rl.question("Podaj swój wiek: ", function(wiek){
+            const user = {
+                imie: imie,
+                nazwisko: nazwisko,
+                wiek: Number(wiek)
+            }
+        })
+    })
+})
