5b66947a23156a1d4cf05ea815ca6b1ede5dc0c9
Grzegorz <grzegorz.gejlo@cyfroweszkoly.pl>
2024-11-02 20:42:34 +0100

    Ulepszenie monitorowania folderu (usuwanie i dodawanie pliku)

--- MonitorFileChanges/FileWatcher.mjs
+++ MonitorFileChanges/FileWatcher.mjs
@@ -1,6 +1,18 @@
 import fs from 'fs'
+import path from 'path'
 
 fs.watch("./watchcatalog", (eventType, filename)=>{
-    console.log(`Zdarzenie typu: ${eventType} w pliku ${filename}`)
+    if(eventType === "rename"){
+        const fullPath = path.join('./watchcatalog', filename)
+        fs.access(fullPath, err=>{
+            if(err){
+                console.log(`Usunięto plik ${filename}`)
+            }else{
+                console.log(`Dodano plik ${filename}`)
+            }
+        })
+    }else if(eventType === "change"){
+        console.log(`zmiany w pliku ${filename}`)
+    }
 })
 console.log("Monitoruje folder watchcatalog")
