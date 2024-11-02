86bc24735303fd299e977a8e7bcfcba7f76bd6b8
Grzegorz <grzegorz.gejlo@cyfroweszkoly.pl>
2024-11-02 18:36:21 +0100

    Dodanie zapyta o dane do obiektu oraz tworzenie obiektu

--- jsonDataManager/jsonDataManager.mjs
+++ jsonDataManager/jsonDataManager.mjs
@@ -8,7 +8,19 @@ const rl = readline.createInterface({
 
 rl.question('Jeśli chcesz dodać nowy obiekt wpisz "dodaj", a jeśli chcesz wyświetlić dane wpisz "wyswietl": ', function(option){
     if(option === "dodaj"){
-        console.log("dodaj")
+        rl.question("Podaj imię: ", function(name){
+            rl.question("Podaj wiek: ", function(age){
+                rl.question("Podaj email: ", function(email){
+                    const user = {
+                        imie: name,
+                        wiek: Number(age),
+                        email: email
+                    }
+                    console.log(user)
+                    rl.close
+                })
+            })
+        })
     }
     if(option ==="wyswietl"){
         console.log("wyświetl")
