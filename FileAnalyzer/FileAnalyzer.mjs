615c6652988c8a82f2356647ca036492653d3456
Grzegorz <grzegorz.gejlo@cyfroweszkoly.pl>
2024-11-02 22:22:36 +0100

    Uzycie eventemitter

--- FileAnalyzer/FileAnalyzer.mjs
+++ FileAnalyzer/FileAnalyzer.mjs
@@ -1,9 +1,16 @@
 import fs from 'fs'
 import path from 'path'
 import EventEmitter from 'events'
+import EventEmitter from 'events'
 
 const readline = require("readline")
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
+
+const EventEmitter = new EventEmitter()
+
+EventEmitter.on('Startfile', (file) => console.log(`Rozpoczęcie analizy pliku: ${file}`))
+EventEmitter.on('Stopfile', (file) => console.log(`Zakończenie analizy pliku: ${file}`))
+
