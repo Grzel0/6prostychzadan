ca1b9ecc7073434325c11338700b701245a42ce2
Grzegorz <grzegorz.gejlo@cyfroweszkoly.pl>
2024-11-02 22:29:09 +0100

    Wykorzystanie petli forEach

--- FileAnalyzer/FileAnalyzer.mjs
+++ FileAnalyzer/FileAnalyzer.mjs
@@ -14,3 +14,17 @@ const EventEmitter = new EventEmitter()
 EventEmitter.on('Startfile', (file) => console.log(`Rozpoczęcie analizy pliku: ${file}`))
 EventEmitter.on('Stopfile', (file) => console.log(`Zakończenie analizy pliku: ${file}`))
 
+rl.question("Podaj ścieżkę do folderu: ", function(pathcatalog){
+    fs.readdir(`./${pathcatalog}`, (err, items)=>{
+        if(err){
+            console.error("Błąd")
+            return
+        }
+        items.forEach((item) =>{
+            const fullPath = path.join(`./${pathcatalog}`, item)
+            fs.stat(fullPath, (err, stats)=>{
+                
+            })
+        })
+    })
+})
