b94dad9318f77abd70991b0a75f97a0538931ec0
Grzegorz <grzegorz.gejlo@cyfroweszkoly.pl>
2024-11-02 22:43:56 +0100

    Wyswietlanie statystyk oraz obsluga bledu

--- FileAnalyzer/FileAnalyzer.mjs
+++ FileAnalyzer/FileAnalyzer.mjs
@@ -1,18 +1,17 @@
 import fs from 'fs'
 import path from 'path'
 import EventEmitter from 'events'
-import EventEmitter from 'events'
+import readline from "readline"
 
-const readline = require("readline")
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-const EventEmitter = new EventEmitter()
+const eventEmitter = new EventEmitter()
 
-EventEmitter.on('Startfile', (file) => console.log(`Rozpoczęcie analizy pliku: ${file}`))
-EventEmitter.on('Stopfile', (file) => console.log(`Zakończenie analizy pliku: ${file}`))
+eventEmitter.on('Startfile', (file) => console.log(`Rozpoczęcie analizy pliku: ${file}`))
+eventEmitter.on('Stopfile', (file) => console.log(`Zakończenie analizy pliku: ${file}`))
 
 rl.question("Podaj ścieżkę do folderu: ", function(pathcatalog){
     fs.readdir(`./${pathcatalog}`, (err, items)=>{
@@ -23,7 +22,15 @@ rl.question("Podaj ścieżkę do folderu: ", function(pathcatalog){
         items.forEach((item) =>{
             const fullPath = path.join(`./${pathcatalog}`, item)
             fs.stat(fullPath, (err, stats)=>{
-                
+                if(err){
+                    console.error("Błąd")
+                    return
+                }
+                if(stats.isDirectory()){
+                    console.log(`${item} - folder`)
+                }else{
+                    console.log(`Plik - ${item}\nRozmiar - ${stats.size}\nRozszerzenie - ${path.extname(item)}\nOstatnia data modyfikacji: ${stats.mtime}\n`)
+                }
             })
         })
     })
